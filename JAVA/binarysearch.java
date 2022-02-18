51937f4379f8d27f823bb63bd2d332bb69a760f7
unknown <19eucs030@skcet.ac.in>
2022-02-18 23:40:38 +0530

    2nd day

---
+++ JAVA/binarysearch.java
@@ -0,0 +1,56 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class binarysearch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("no of array elements");
+        int n=sc.nextInt();
+        //declare array
+        int[] arr= new int[n];
+        System.out.println("array values");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("value to search");
+        int num=sc.nextInt();
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        
+        while(low<=high){
+            mid=(low+high)/2;
+            if(arr[mid]==mid){
+                System.out.println("value at index"+mid);
+                break;
+            }
+            else if(arr[mid]>num){
+                high=mid-1;
+            }
+            else if(arr[mid]<num){
+                low=mid+1;
+            }
+        }
+        if(low>high){
+            System.out.println("Value not found");
+        }
+
+    }
+}
+/*
+low=0 , high =size-1, search=x
+
+while(low<=high){
+    mid=(low+high)/2;
+    if(arr[mid]==search){
+        return
+    }
+    else if(search>ar[mid]){
+        low=mid+1;
+    }
+    else if(search<ar[mid]){
+        high=mid-1;
+    }
+}
+*/
---
+++ JAVA/perfectnum.java
@@ -0,0 +1,29 @@
+package JAVA;
+import java.util.*;
+
+public class perfectnum {
+    public static void main(String[] args){
+        int sum=0;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num>0){
+
+            for(int i=1; i<num; i++){
+                if(num%i==0){
+                    sum=sum+i;
+                }
+            }
+            if(sum==num){
+                System.out.println("Perfect Number");
+            }
+            else{
+                System.out.println("Not Perfect Number");
+            }
+        }
+        else{
+            System.out.println("negative value");
+        }
+
+    }
+}
+//perfect num is positive interger which is equal to the sum of positive divisor(factors)
